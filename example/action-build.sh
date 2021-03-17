7e2752f2c19c0c611f219d68fe5d9f280817a753
Calin Radoni <CalinRadoni@users.noreply.github.com>
2021-03-17 15:40:52 +0200

    Updates the GitHub Action for building the example

--- example/action-build.sh
+++ example/action-build.sh
@@ -5,7 +5,7 @@ declare -i res
 . ${IDF_PATH}/export.sh
 
 export CodeLib_PATH="/github/workspace/lib"
-cd /github/workspace/app/example
+cd /github/workspace/ESP32DLEDController/example
 
 idf.py build
 res=$?
